9e163383660b80cd65c8177b9f26da7709d462b2
Baran Ates <baran.ates@accenture.com>
2025-08-16 18:19:11 +0300

    mockpopupmanager added

--- test/helper.dart
+++ test/helper.dart
@@ -2,6 +2,7 @@
 
 import 'package:mocktail/mocktail.dart';
 import 'package:plant_app/core/utils/logger.dart';
+import 'package:plant_app/core/utils/popup_manager.dart';
 import 'package:plant_app/features/auth/domain/repositories/auth_repository.dart';
 import 'package:plant_app/features/home/domain/repositories/home_repository.dart';
 
@@ -10,3 +11,5 @@ class MockAuthRepository extends Mock implements AuthRepository {}
 class MockHomeRepository extends Mock implements HomeRepository {}
 
 class MockLogger extends Mock implements Logger {}
+
+class MockPopupManager extends Mock implements PopupManager {}
